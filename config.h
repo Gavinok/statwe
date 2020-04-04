cdd89c49055721f014764577df1aaf57809c789e
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-04-04 15:55:29 -0600

    Slowed refresh to 2 seconds

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* set static variables */
 static const char bataddress[] = "BAT0"; // battery to get status from 
-static int sleeptime = 1; // time to sleep in secounds
+static int sleeptime = 2; // time to sleep in secounds
 
 /* brightness settings */
 static const char *cur_brightness = "/sys/class/backlight/intel_backlight/brightness";
